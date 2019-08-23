c673e20938b8ff4d41228ab16455e188d1179002
jonathanmiller2 <jonathanmiller2@hotmail.com>
2019-08-23 13:20:54 -0500

    Finished Player Movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+
+	[SerializeField]
+	private Camera cam;
+
+	[SerializeField]
+	private float speed = 5f;
+
+	[SerializeField]
+	private float lookSensitivity = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Calculate movement velocity as a 3D vector
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMov;
+        Vector3 moveVertical = transform.forward * zMov;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        if(velocity != Vector3.zero)
+        {
+        	rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+        //Calculate rotation as a 3D vector and apply
+        float yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * lookSensitivity;
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+
+        //Calculate camera tilt as a 3D vector and apply
+        float xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 cameraTilt = new Vector3(-xRot, 0f, 0f) * lookSensitivity;
+        cam.transform.Rotate(cameraTilt);
+        
+
+    }
+
+
+}
